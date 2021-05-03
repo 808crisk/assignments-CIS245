c2930c28945357e251f78bb220bbc2cab8db4432
808crisk <83427691+808crisk@users.noreply.github.com>
2021-05-02 19:10:07 -0700

    Add files via upload

---
+++ class project ck.py
@@ -0,0 +1,20 @@
+# modules
+import requests
+
+# request for data
+def weatherInfo(query):
+   # Enter your API key here
+   ApiKey = " 324eae13d8d2460871b2965b1eecb4fa"
+   # Open Weather API
+   WeatherSite = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}?"
+   
+# display results
+def DisplayResults(weathers,city):
+   #print("City name and temperature")
+   #print("Weather info)
+   
+# main function
+def main():
+  #allow user to enter multi city/zip code queries
+  #
+	
